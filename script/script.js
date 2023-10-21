13f58314dbb7d461b07955544753df473a5c1a2e
normand <aymon.normand@gmail.com>
2023-10-21 23:46:43 +0200

    update

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+// Querry selector of sidebar
 const sidebarLinks = document.querySelectorAll('.sidebar-link');
 const sections = {
   welcome: document.querySelector('.main > .welcome'),
@@ -9,15 +10,15 @@ const sections = {
   moi: document.querySelector('.moi'),
 };
 
-// Function to set the active link and background color
+// acivate or deactivate backgroud color of main
 function setActiveLink(activeLinkId) {
   sidebarLinks.forEach((link) => {
     if (link.id === activeLinkId) {
       link.classList.add('active');
-      link.style.backgroundColor = '#1165a15d'; // Set the background color
+      link.style.backgroundColor = '#1165a15d';
     } else {
       link.classList.remove('active');
-      link.style.backgroundColor = ''; // Remove the background color
+      link.style.backgroundColor = '';
     }
   });
   for (const key in sections) {
@@ -27,29 +28,28 @@ function setActiveLink(activeLinkId) {
 }
 }
 
-// Add a click event listener to each sidebar link
+// click on sidebar
 sidebarLinks.forEach((link) => {
   link.addEventListener('click', () => {
     const id = link.id;
     
-    // Check if the section is already visible
+    // see if main page is visible or not
     if (sections[id].style.display === 'grid') {
       sections[id].style.display = 'none';
       sections.welcome.style.display = 'grid';
     } else {
-      // Hide all sections
+      // display none to all other main page
       for (const key in sections) {
         sections[key].style.display = 'none';
       }
-      // Show the corresponding section
+      // display grid for selected main page
       sections[id].style.display = 'grid';
-      // Set the active link and background color
       setActiveLink(id);
     }
   });
 });
 
-// Initially hide all sections except the welcome section
+// set welcome as default
 for (const key in sections) {
   if (key !== 'welcome') {
     sections[key].style.display = 'none';
@@ -59,7 +59,7 @@ for (const key in sections) {
 sidebarLinks.forEach((link) => {
   link.style.backgroundColor = 'transparent';
 })
-
+// if click on header = refresh the page, back to welcome
 document.addEventListener('DOMContentLoaded', function() {
   var header = document.querySelector('.header');
   header.addEventListener('click', function(event) {
@@ -68,7 +68,7 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 });
 
-
+// try to make an effect follow the mouse... failed...
 document.addEventListener('mousemove', e => {
   const elements = document.querySelectorAll(".autodidacte.projet.competences.formations.experiences.moi");
 
@@ -82,8 +82,8 @@ document.addEventListener('mousemove', e => {
   }
 });
 
-// Select all elements with the specified classes and attach the mousemove event handler
+
 const cards = document.querySelectorAll(".autodidacte.projet.competences.formations.experiences.moi");
 for (const card of cards) {
-  card.onmousemove = e => handleOnMouseMove(e); // Corrected function name here
+  card.onmousemove = e => handleOnMouseMove(e);
 }
